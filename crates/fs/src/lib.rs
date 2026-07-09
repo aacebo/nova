62131499b0349848d1668d2bae89e4db68fe4d80
aacebo <aacebowork@gmail.com>
2026-07-09 19:36:19 -0400

    Update lib.rs

--- crates/fs/src/lib.rs
+++ crates/fs/src/lib.rs
@@ -8,19 +8,20 @@ impl nova::Import for FileSystem {
 
 pub fn read(args: &nova::Args, _scope: &nova::Scope) -> Result<nova::Value, Box<dyn std::error::Error>> {
     let path = args.at(0);
+    let base = std::env::current_dir()?;
     let path = path.as_str().ok_or(nova::Error::message("path must be a string"))?;
-    let data = std::fs::read_to_string(path)?;
+    let data = std::fs::read_to_string(base.join(path))?;
     Ok(data.into())
 }
 
 pub fn write(args: &nova::Args, _scope: &nova::Scope) -> Result<(), Box<dyn std::error::Error>> {
     let path = args.at(0);
+    let base = std::env::current_dir()?;
     let path = path.as_str().ok_or(nova::Error::message("path must be a string"))?;
-
     let data = args.at(1);
 
     if let Some(data) = data.as_bytes() {
-        Ok(std::fs::write(path, data)?)
+        Ok(std::fs::write(base.join(path), data)?)
     } else {
         Err(Box::new(nova::Error::message("invalid data type")))
     }
