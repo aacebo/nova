f3e240b67335e7c2cff4d1315437a53493fe7146
aacebo <aacebowork@gmail.com>
2026-07-14 16:57:59 -0400

    stuff

--- crates/reflect/src/dynamic/dyn.rs
+++
@@ -1,4 +0,0 @@
-/// ## Dyn
-///
-/// implemented by types that can reflect their value/type.
-pub trait Dyn: std::fmt::Debug + Send + Sync + crate::ToType {}
--- crates/reflect/src/dynamic/mod.rs
+++ crates/reflect/src/dynamic/mod.rs
@@ -1,41 +1,33 @@
 mod callable;
-mod r#dyn;
 mod object;
 mod sequence;
 
 pub use callable::*;
-pub use r#dyn::*;
 pub use object::*;
 pub use sequence::*;
 
 #[derive(Debug, Clone)]
 pub enum Dynamic<'a> {
-    Dyn(&'a (dyn crate::Dyn + 'a)),
-    Object(&'a (dyn crate::Object + 'a)),
-    Sequence(&'a (dyn crate::Sequence + 'a)),
-    Callable(&'a (dyn crate::Callable + 'a)),
+    Object(&'a (dyn Object + 'a)),
+    Sequence(&'a (dyn Sequence + 'a)),
+    Callable(&'a (dyn Callable + 'a)),
 }
 
 impl<'a> Dynamic<'a> {
-    pub fn from_dyn<T: crate::Dyn + 'a>(value: &'a T) -> Self {
-        Self::Dyn(value)
-    }
-
-    pub fn from_object<T: crate::Object + 'a>(value: &'a T) -> Self {
+    pub fn from_object<T: Object + 'a>(value: &'a T) -> Self {
         Self::Object(value)
     }
 
-    pub fn from_sequence<T: crate::Sequence + 'a>(value: &'a T) -> Self {
+    pub fn from_sequence<T: Sequence + 'a>(value: &'a T) -> Self {
         Self::Sequence(value)
     }
 
-    pub fn from_callable<T: crate::Callable + 'a>(value: &'a T) -> Self {
+    pub fn from_callable<T: Callable + 'a>(value: &'a T) -> Self {
         Self::Callable(value)
     }
 
     pub fn to_type(&self) -> crate::Type {
         match self {
-            Self::Dyn(v) => v.to_type(),
             Self::Object(v) => v.to_type(),
             Self::Sequence(v) => v.to_type(),
             Self::Callable(v) => v.to_type(),
@@ -72,21 +64,21 @@ impl<'a> Dynamic<'a> {
         matches!(self, Self::Callable(_))
     }
 
-    pub fn as_object(&self) -> Option<&'a (dyn crate::Object + 'a)> {
+    pub fn as_object(&self) -> Option<&'a (dyn Object + 'a)> {
         match self {
             Self::Object(v) => Some(*v),
             _ => None,
         }
     }
 
-    pub fn as_sequence(&self) -> Option<&'a (dyn crate::Sequence + 'a)> {
+    pub fn as_sequence(&self) -> Option<&'a (dyn Sequence + 'a)> {
         match self {
             Self::Sequence(v) => Some(*v),
             _ => None,
         }
     }
 
-    pub fn as_callable(&self) -> Option<&'a (dyn crate::Callable + 'a)> {
+    pub fn as_callable(&self) -> Option<&'a (dyn Callable + 'a)> {
         match self {
             Self::Callable(v) => Some(*v),
             _ => None,
@@ -103,7 +95,6 @@ impl<'a> crate::TypeOf for Dynamic<'a> {
 impl<'a> crate::ToType for Dynamic<'a> {
     fn to_type(&self) -> crate::Type {
         match self {
-            Self::Dyn(v) => v.to_type(),
             Self::Object(v) => v.to_type(),
             Self::Sequence(v) => v.to_type(),
             Self::Callable(v) => v.to_type(),
@@ -117,7 +108,7 @@ impl<'a> crate::ToValue for Dynamic<'a> {
     }
 }
 
-impl<'a> crate::Object for Dynamic<'a> {
+impl<'a> Object for Dynamic<'a> {
     fn field(&self, name: &str) -> crate::Value<'_> {
         match self {
             Self::Object(v) => v.field(name),
@@ -133,7 +124,7 @@ impl<'a> crate::Object for Dynamic<'a> {
     }
 }
 
-impl<'a> crate::Sequence for Dynamic<'a> {
+impl<'a> Sequence for Dynamic<'a> {
     fn len(&self) -> usize {
         match self {
             Self::Sequence(v) => v.len(),
@@ -149,7 +140,7 @@ impl<'a> crate::Sequence for Dynamic<'a> {
     }
 }
 
-impl<'a> crate::Callable for Dynamic<'a> {
+impl<'a> Callable for Dynamic<'a> {
     fn call(&self, args: &[crate::Value]) -> Result<crate::Value<'_>, String> {
         match self {
             Self::Callable(v) => v.call(args),
@@ -174,7 +165,7 @@ impl<'a> serde::Serialize for Dynamic<'a> {
         use serde::ser::SerializeMap;
 
         match self {
-            Self::Dyn(_) | Self::Callable(_) => serializer.serialize_none(),
+            Self::Callable(_) => serializer.serialize_none(),
             Self::Object(v) => {
                 let ty = v.to_type().to_struct();
                 let fields = ty.as_ref().map(|t| t.fields());
